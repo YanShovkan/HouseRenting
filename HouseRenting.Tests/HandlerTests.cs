5bd002f5e52d4956a90ac2a404961a9852ba68e0
yan shovkan <yan.shovkan@nordclan.com>
2024-05-15 14:50:02 +0400

    тесты

---
+++ HouseRenting.Tests/HandlerTests.cs
@@ -0,0 +1,135 @@
+﻿namespace HouseRenting.Tests;
+public class HandlerTests
+{
+    [Fact]
+    public void UserHandler_Add_UserLoginNotContains_ShouldAddUser()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void UserHandler_Add_UserLoginContains_ShouldThrowException()
+    {        
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void UserHandler_Update_ContainsAllRequiredProperties_ShouldUpdate()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void UserHandler_Delete_HasUserWithId_ShouldDelete()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void UserHandler_GetOne_HasUserWithId_ShouldReturnUserDto()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void UserHandler_GetList_HasUser_ShouldReturnUsersListDto()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void AdminHandler_Add_AdminLoginNotContains_ShouldAddAdmin()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void AdminHandler_Add_AdminLoginContains_ShouldThrowException()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void AdminHandler_Update_ContainsAllRequiredProperties_ShouldUpdate()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void AdminHandler_Delete_HasAdminWithId_ShouldDelete()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void AdminHandler_GetOne_HasAdminWithId_ShouldReturnAdminDto()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void AdminHandler_GetList_HasAdmin_ShouldReturnAdminsListDto()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void OrderHandler_Add_ContainsAllRequiredProperties_ShouldAddOrder()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void OrderHandler_Update_ContainsAllRequiredProperties_ShouldUpdate()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void OrderHandler_Delete_HasOrderWithId_ShouldDelete()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void OrderHandler_GetOne_HasOrderWithId_ShouldReturnOrderDto()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void OrderHandler_GetList_HasOrder_ShouldReturnOrderListDto()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void AdvertHandler_Add_ContainsAllRequiredProperties_ShouldAddAdvert()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void AdvertHandler_Update_ContainsAllRequiredProperties_ShouldUpdate()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void AdvertHandler_Delete_HasAdvertWithId_ShouldDelete()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void AdvertHandler_GetOne_HasAdvertWithId_ShouldReturnAdvertDto()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void AdvertHandler_GetList_HasAdvert_ShouldReturnAdvertsListDto()
+    {
+        Assert.True(true);
+    }
+}
---
+++ HouseRenting.Tests/ServiceTests.cs
@@ -0,0 +1,50 @@
+namespace HouseRenting.Tests;
+
+public class ServiceTests
+{
+    [Fact]
+    public void EmailSender_SendEmailAdoutMeet_NewMeet_ShouldSendEmail()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void EmailSender_SendEmailAdoutMeet_NewMeetTime_ShouldSendEmail()
+    {
+        Assert.True(true);
+    }
+
+
+    [Fact]
+    public void PredictService_GetPredict_TwoYears_ShouldReturnPredictUsingLastOneYear()
+    {
+        Assert.True(true);
+    }
+
+
+    [Fact]
+    public void PredictService_GetPredict_OneMonths_ShouldThrowException()
+    {
+        Assert.True(true);
+    }
+
+
+    [Fact]
+    public void PredictService_GetPredict_SixMonths_ShouldReturnPredict()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void ReportService_GetReport_ShouldReturnFile()
+    {
+        Assert.True(true);
+    }
+
+
+    [Fact]
+    public void StatisticCollector_GetStatistic_ShouldReturnStatistic()
+    {
+        Assert.True(true);
+    }
+}
